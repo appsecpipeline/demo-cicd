07876a88e584b55c7467425fb5377a629c58de13
Matt Tesauro <mtesauro@gmail.com>
2024-06-12 23:55:29 -0500

    Update admin.py
    Minor update

--- project/boh/admin.py
+++ project/boh/admin.py
@@ -4,7 +4,7 @@ from django.utils.html import format_html
 
 from . import models
 
-# Comment
+# Comment changed
 
 class EnvironmentCredentials(admin.StackedInline):
     model = models.EnvironmentCredentials
